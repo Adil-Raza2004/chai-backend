c63df6fb3116748b86435568bfd95cb0ae92087b
Adil Raza <razaadil00004@gmail.com>
2025-07-02 03:25:21 +0530

    Your message here

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+//while connecting to db we always need to write need so write this  part so write a method here and export it 
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
